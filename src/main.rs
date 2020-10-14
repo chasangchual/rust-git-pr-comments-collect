603a10bb420753cf65f382ce811014a199081e4c
Sangchual Cha <scha@cwretirement.com>
2020-10-14 01:07:27 -0400

    break down pull request lists

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ async fn collect_prs2() -> Result<(), Error> {
 
     let response = Client::new()
     .get("https://api.github.com/repos/apache/kafka/pulls")
-    .bearer_auth("dbf116503a6ebe6c207d4c6564f0f4bb53abd687")
+    .bearer_auth("49c2770553f3811abc70c8cb9e82525838afab98")
     .header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36")
     .send().await?;
 
@@ -27,7 +27,20 @@ async fn collect_prs2() -> Result<(), Error> {
 }
 
 fn parse_json(json_str: String) {
-    let parsed: Value = from_str(&json_str).unwrap();    
-    println!("{:?}", parsed);
+//    println!("{}", json_str);
 
+    let parsed: Value = from_str(&json_str).unwrap();    
+    println!("is_array: {:?}", parsed.is_array());
+    println!("is_object: {:?}", parsed.is_object());
+
+    if parsed.is_array() {
+        for obj in parsed.as_array().unwrap() {
+            println!("{}", obj);
+            println!("\n\n");
+        }
+    } else if parsed.is_object() {
+        for key in parsed.as_object().unwrap().keys() {
+            println!("{}", key);
+        }
+    }
 }
