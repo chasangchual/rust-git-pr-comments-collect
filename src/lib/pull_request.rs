cf52f9a71b8ea718027f644c2f2eb2214dbcdab1
Sangchual Cha <scha@cwretirement.com>
2020-10-15 23:20:46 -0400

    seprate functions and put them under lib directory

---
+++ src/lib/pull_request.rs
@@ -0,0 +1,44 @@
+use reqwest::Error;
+use reqwest::Client;
+use serde_json::from_str;
+use serde_json::Value;
+
+pub async fn collect_pull_request(owner: &str, repository: &str) -> Result<(), Error> {
+//    let endpoint = format!("https://api.github.com/repos/apache/kafka/pulls")
+    let endpoint = format!("https://api.github.com/repos/{owner}/{repository}/pulls", owner=owner, repository=repository);
+
+    let response = Client::new()
+    .get(&endpoint)
+    .bearer_auth("52fbd36b149fcdd2fcacc7f00f73bda4c340349c")
+    .header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36")
+    .send().await?;
+
+    let out = response.text().await;
+
+    match out {
+        Ok(body) => parse_json(body),
+        Err(e) => println!("{:?}", e)
+    };
+    
+    Ok(())
+}
+    
+    
+pub fn parse_json(json_str: String) {
+    //    println!("{}", json_str);
+    
+    let parsed: Value = from_str(&json_str).unwrap();    
+    println!("is_array: {:?}", parsed.is_array());
+    println!("is_object: {:?}", parsed.is_object());
+
+    if parsed.is_array() {
+        for obj in parsed.as_array().unwrap() {
+            println!("{}", obj);
+            println!("\n\n");
+        }
+    } else if parsed.is_object() {
+        for node in parsed.as_object().unwrap() {
+            println!("{:?}", node);
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,12 @@
-use reqwest::Error;
-use reqwest::Client;
-use serde_json::from_str;
-use serde_json::Value;
+
+mod lib;
+
+use lib::pull_request::collect_pull_request;
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
-    collect_prs2().await
-}
+async fn main() {
+    let owner = "apache";
+    let repository = "kafka";
 
-async fn collect_prs2() -> Result<(), Error> {
-
-    let response = Client::new()
-    .get("https://api.github.com/repos/apache/kafka/pulls")
-    .bearer_auth("49c2770553f3811abc70c8cb9e82525838afab98")
-    .header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36")
-    .send().await?;
-
-    let out = response.text().await;
-
-    match out {
-        Ok(v) =>parse_json(v),
-        Err(e) => println!("{:?}", e)
-    }
-
-    Ok(())
-}
-
-fn parse_json(json_str: String) {
-//    println!("{}", json_str);
-
-    let parsed: Value = from_str(&json_str).unwrap();    
-    println!("is_array: {:?}", parsed.is_array());
-    println!("is_object: {:?}", parsed.is_object());
-
-    if parsed.is_array() {
-        for obj in parsed.as_array().unwrap() {
-            println!("{}", obj);
-            println!("\n\n");
-        }
-    } else if parsed.is_object() {
-        for key in parsed.as_object().unwrap().keys() {
-            println!("{}", key);
-        }
-    }
+    collect_pull_request(owner, repository).await.unwrap();
 }
