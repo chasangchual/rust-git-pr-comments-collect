8925babe59b80ae6d80fa7619bcb4b87c69571b3
Sangchual Cha <sangchual.cha@hotmail.com>
2020-10-18 23:37:30 -0400

    implement Repository struct

--- src/lib/entities.rs
+++ src/lib/entities.rs
@@ -1,33 +1,58 @@
-use chrono::DateTime;
+use chrono::{DateTime, TimeZone, Utc};
 
-struct Repository {
-    _pid: i32;
+pub struct Repository {
+    _pid: i32,
     owner: String,
     repository: String,
-    created_at: DateTime,
-    updated_at: DateTime,
+    created_at: Option<DateTime<Utc>>,
+    updated_at: Option<DateTime<Utc>>,
 }
 
-struct PullRequest {
-    _pid: i32;
-    repository_pid: i32;
-    number: i32;
+pub struct PullRequest {
+    _pid: i32,
+    repository_pid: i32,
+    number: i32,
     endpoint: String,
     title: String,
     body: String,
-    created_at: DateTime,
-    updated_at: DateTime,
+    created_at: Option<DateTime<Utc>>,
+    updated_at: Option<DateTime<Utc>>,
 }
 
-struct Comments {
-    _pid: i32;
-    pr_pid: i32;
-    number: i32;
+pub struct Comments {
+    _pid: i32,
+    pr_pid: i32,
+    number: i32,
     endpoint: String,
     body: String,
     diff_hunk: String,
     path: String,
     html_url: String,
-    created_at: DateTime,
-    updated_at: DateTime,
+    created_at: Option<DateTime<Utc>>,
+    updated_at: Option<DateTime<Utc>>,
+}
+
+
+impl Repository {
+    pub fn new(pid: i32, owner: String, repository: String) -> Repository {
+        Repository {
+            _pid: pid,
+            owner: owner,
+            repository: repository,
+            created_at: None,
+            updated_at: None,
+        }
+    }
+
+    // Immutable access.
+    pub fn pid(&self) -> &i32 {
+        &self._pid
+    }
+    pub fn owner(&self) -> &String {
+        &self.owner
+    }
+
+    pub fn repository(&self) -> &String {
+        &self.repository
+    }
 }
