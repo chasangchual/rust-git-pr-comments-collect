c760a1c732daad9822fc00ff68cac20204a32c73
Sangchual Cha <sangchual.cha@hotmail.com>
2020-10-29 02:03:16 -0400

    user HeaerMap to specify User-Agent header

--- src/rest_client/base_client.rs
+++ src/rest_client/base_client.rs
@@ -1,4 +1,4 @@
-use reqwest::header::{HeaderMap, HeaderName, HeaderValue, ACCEPT, CONTENT_TYPE};
+use reqwest::header::{HeaderMap, HeaderName, HeaderValue, ACCEPT, CONTENT_TYPE, USER_AGENT};
 use reqwest::Error;
 use reqwest::blocking::{Client, Response};
 use serde::Serialize;
@@ -8,26 +8,31 @@ use std::env;
 pub struct BaseClient {
     client: Client,
     api_token: String,
-    user_agent: String,
+    headers: HeaderMap,
 }
 
 impl BaseClient {
     pub fn new() -> BaseClient {
         let token: String = env::var("GITHUB_API_TOKEN").expect("GITHUB_API_TOKEN must set");
+        let mut headers = HeaderMap::new();
 
+        headers.insert(USER_AGENT, HeaderValue::from_static("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"));
+        
         BaseClient {
             client: reqwest::blocking::Client::new(),
             api_token: token,
-            user_agent: String::from("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"),
+            headers: headers,
         }
     }
 
     /// Get a resource
     pub fn get(&self, endpoint: &str) -> Result<Response, Error> {
+        let headers = self.headers.clone();
+
         match self.client
         .get(endpoint)
         .bearer_auth(&(self.api_token.as_str()))
-        .header("User-Agent", self.user_agent.as_str())
+        .headers(headers)
         .send() {
             Ok(response) => Ok(response),
             Err(error) => Err(error),
