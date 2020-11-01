ebc6caf80208480d85e054bb92b0b058fc084c7a
Sangchual Cha <sangchual.cha@hotmail.com>
2020-10-31 23:28:56 -0400

    create DBClient struct

--- src/db/connection.rs
+++ src/db/connection.rs
@@ -1,9 +1,19 @@
-#[macro_use]
 use diesel::prelude::*;
 use diesel::pg::PgConnection;
 use std::env;
 
-pub fn establish_connection() -> PgConnection {
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must set");
-    PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+pub struct DBClient {
+    client: PgConnection,
+    url: String,
+}
+
+impl DBClient {
+    pub fn new() -> DBClient {
+        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must set");
+
+        DBClient {
+            url: database_url.clone(),
+            client: PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+        }
+    }
 }
