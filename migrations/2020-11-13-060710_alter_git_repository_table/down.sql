801e4f8411911566d8296ac1c3597247737098fb
Sangchual Cha <sangchual.cha@hotmail.com>
2020-11-15 01:45:34 -0500

    extend git_repository schema and model

--- migrations/2020-11-13-060710_alter_git_repository_table/down.sql
+++ migrations/2020-11-13-060710_alter_git_repository_table/down.sql
@@ -1,1 +1,15 @@
 -- This file should undo anything in `up.sql`
+ALTER TABLE public.git_repository DROP COLUMN "number";
+ALTER TABLE public.git_repository DROP COLUMN full_name;
+ALTER TABLE public.git_repository DROP COLUMN private;
+ALTER TABLE public.git_repository DROP COLUMN description;
+ALTER TABLE public.git_repository DROP COLUMN "language";
+ALTER TABLE public.git_repository DROP COLUMN url;
+ALTER TABLE public.git_repository DROP COLUMN "size";
+ALTER TABLE public.git_repository DROP COLUMN stargazers_count;
+ALTER TABLE public.git_repository DROP COLUMN watchers_count;
+ALTER TABLE public.git_repository DROP COLUMN forks_count;
+ALTER TABLE public.git_repository DROP COLUMN open_issues_count;
+ALTER TABLE public.git_repository DROP COLUMN open_issues;
+ALTER TABLE public.git_repository DROP COLUMN watchers;
+ALTER TABLE public.git_repository DROP COLUMN subscribers_count;
